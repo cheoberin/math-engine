afd175f44133e6f21a462bed47f1e0f00061ce91
Airton Ribeiro Manfrini Junior <airton.manfrini@hotmail.com>
2026-04-20 11:26:00 -0300

    First Commit

---
+++ src/main/java/com/cheobs/math_engine/MathEngineApplication.java
@@ -0,0 +1,13 @@
+package com.cheobs.math_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cheobs/math_engine/MathEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheobs.math_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
